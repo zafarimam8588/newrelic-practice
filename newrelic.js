0e8282cb914bc8e0b46de34ea3ad3e9bd2c0c5fe
Zafar Imam <zimam8588@gmail.com>
2024-08-10 23:39:08 +0530

    added newrelic.js

---
+++ newrelic.js
@@ -0,0 +1,8 @@
+exports.config = {
+  app_name: ["new-relic-practice"],
+  license_key: "a9ca3cd8c0f2c607a7a8de1c7c5d2b14FFFFNRAL",
+  logging: {
+    level: "info", // You can adjust this as needed
+  },
+  // other configurations...
+};
